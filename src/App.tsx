cd664cb263cc778ecad58bf0409f59535f4f4f24
Query-Interface <->
2019-01-15 00:13:09 +0100

    Initial import

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { Layout, Menu, Icon } from 'antd';
+import {SiderSubMenu} from './components/SiderSubMenu';
+
+const { Header, Content, Footer, Sider } = Layout;
+//const { SubMenu } = Menu;
+
+const boards = [
+    {"id": 12, "title": "Backlog Items", "icon": "project"},
+    {"id": 13, "title": "Todos", "icon": "book"}
+];
+/*const subMenu1 = {
+    "id": "subBoards",
+    "menuEntries":boards,
+    "icon": "project",
+    "title": "Boards"
+};*/
+
+export class App extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+          collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
+      return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            trigger={null}
+            >
+                <div className="logo" style={{color:'white'}}>OPEN TRELLO</div>
+                    <SiderSubMenu id="subBoards" title="Boards" icon="project" menuEntries={boards} />
+            </Sider>
+        <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+                <div>
+                    <Icon
+                        className="trigger"
+                        type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                        onClick={this.toggle}
+                    />
+                    <Menu
+                        theme="light"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px' }}
+                        className="right"
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </div>
+            </Header>
+            <Content style={{
+            margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
+            }}
+            >
+            My board
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by Ant UED
+            </Footer>
+        </Layout>
+      </Layout>
+      );
+   }
+}
---
+++ src/components/SiderSubMenu.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Menu, Icon } from 'antd';
+const { SubMenu } = Menu;
+
+export interface MenuProps {
+    id: number,
+    icon?: string,
+    title: string
+}
+
+export interface SiderMenuProps {
+    id: string;
+    icon?: string;
+    title: string;
+    menuEntries: Array<MenuProps>
+}
+
+export class SiderSubMenu extends React.Component<SiderMenuProps, {}> {
+    
+    constructor (props: SiderMenuProps){
+        super(props);
+    }
+
+    render() {
+        return <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <SubMenu key={this.props.id}
+            title={<span><Icon type={this.props.icon} /><span>{this.props.title}</span></span>}>
+            {this.props.menuEntries.map(function(value, index) {
+                <Menu.Item key={value.id}>
+                {value.title}
+                </Menu.Item>
+            })
+            }
+            </SubMenu>
+        </Menu>
+    }
+}
